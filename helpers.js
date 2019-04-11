7102d7bc90e7afa6e430fec5db4dde94299dbe94
Danial Nickford <admin@redsparr0w.com>
2019-04-12 10:27:16 +1200

    rename getShinyStatusList and remove check if text, as already filtered

--- helpers.js
+++ helpers.js
@@ -40,50 +40,47 @@ function getSymbolFromDate(date){
     return statusSymbols.danger;
 }
 
-function getLatestShinyList(guild){
+function getShinyStatusList(guild){
   const isMatch = /^Most Recent (Sighting|Egg Hatch): (\w{3,9} \d{1,2}, \d{2,4})$/;
   const pokemonList = {};
   const channels = guild.channels.filter(c => c.type == 'text');
   let i = 0;
   return new Promise(function(resolve, reject) {
     channels.forEach(channel => {
-      if (channel.type === "text"){
-        channel.fetchMessages({
-        	limit: 100 // Fetch last 50 messages.
-        }).then((msgCollection) => { // Resolve promise
-        	msgCollection.forEach((msg) => { // forEach on message collection
-        		if (msg.pinned == true && isMatch.test(msg.content)){
-              //console.log(channel.name.replace(/-[^-]+$/, ':') + msg.content.replace(/^.+\:/, ''))
-              const date = new Date(Date.parse(msg.content.match(isMatch)[2]))
-              const name = channel.name.replace(/-[^-]+$/, '');
-              pokemonList[name] = {
-                date,
-                dateStr: msg.content.match(isMatch)[2],
-                symbol: getSymbolFromDate(date),
-                channel: '' + channel,
-                channelName: channel.name,
-              }
-              //channel.edit({ name: name + '-' + pokemonList[name].symbol });
-              return;
+      channel.fetchMessages({
+      	limit: 100 // Fetch last 100 messages.
+      }).then((msgCollection) => {
+      	msgCollection.forEach((msg) => {
+      		if (msg.pinned == true && isMatch.test(msg.content)){
+            const date = new Date(Date.parse(msg.content.match(isMatch)[2]))
+            const name = channel.name.replace(/-[^-]+$/, '');
+            pokemonList[name] = {
+              date,
+              dateStr: msg.content.match(isMatch)[2],
+              symbol: getSymbolFromDate(date),
+              channel: '' + channel,
+              channelName: channel.name,
             }
-        	});
-          if (++i >= channels.size){
-            resolve(pokemonList);
-            debug("Sent latest shiny list");
+            //channel.edit({ name: name + '-' + pokemonList[name].symbol });
+            return;
           }
-        }).catch(e => {
-          if (++i >= channels.size){
-            resolve(pokemonList);
-            debug("Sent latest shiny list");
-          }
-          switch (e.message){
-            case 'Missing Access':
-              break;
-            default:
-              error('Failed to fetch messages for this channel:\n', e);
-          }
-        });
-      }
+      	});
+        if (++i >= channels.size){
+          resolve(pokemonList);
+          debug("Sent latest shiny list");
+        }
+      }).catch(e => {
+        if (++i >= channels.size){
+          resolve(pokemonList);
+          debug("Sent latest shiny list");
+        }
+        switch (e.message){
+          case 'Missing Access':
+            break;
+          default:
+            error('Failed to fetch messages for this channel:\n', e);
+        }
+      });
     });
   });
   debug("Fetching latest shiny list");
@@ -93,5 +90,5 @@ module.exports = {
   splitter,
   statusSymbols,
   getSymbolFromDate,
-  getLatestShinyList,
+  getShinyStatusList,
 }
