1724bb103e3246e778feebff70ab5c193fa150af
Danial Nickford <admin@redsparr0w.com>
2019-05-31 11:12:47 +1200

    formatting

--- helpers.js
+++ helpers.js
@@ -65,7 +65,7 @@ function getSymbolFromDate(date){
 async function updateChannelNames(guild, pokemonList){
   debug('Updating channel names...');
   pokemonList = pokemonList || await getShinyStatusList(guild);
-  const channels = guild.channels.filter(c => c.type == 'text');
+  const channels = guild.channels.filter(channel => channel.type == 'text');
   channels.forEach(channel => {
     const pokemonName = channel.name.replace(/[-\W]+$/, '');
     if (pokemonName in pokemonList && !channel.name.includes(pokemonList[pokemonName].symbol)){
@@ -80,51 +80,52 @@ function getShinyStatusList(guild){
   debug("Fetching latest shiny list");
   const isMatch = /^Most Recent (Sighting|Egg Hatch): (\w{3,9} \d{1,2}, \d{2,4})$/;
   const pokemonList = {};
-  const channels = guild.channels.filter(c => c.type == 'text');
+  const channels = guild.channels.filter(channel => channel.type == 'text');
   let i = 0;
   return new Promise(function(resolve, reject) {
-    channels.filter(channel => channel.name != channel.name.replace(/[-\W]+$/, '')).forEach(channel => {
-      channel.fetchMessages({
-      	limit: 100 // Fetch last 100 messages.
-      }).then((messages) => {
-        const name = channel.name.replace(/[-\W]+$/, '');
-        pokemonList[name] = {
-          channel: '' + channel,
-          channelName: channel.name,
-          dateStr: '',
-          symbol: statusSymbols['unconfirmed'],
-        }
-      	messages.forEach((msg) => {
-      		if (msg.pinned == true && isMatch.test(msg.content)){
-            const date = new Date(Date.parse(msg.content.match(isMatch)[2]))
-            pokemonList[name] = {
-              ...pokemonList[name],
-              ...{
-                date,
-                dateStr: msg.content.match(isMatch)[2],
-                symbol: getSymbolFromDate(date),
-              },
-            }
-            return;
+    channels.filter(channel => channel.name != channel.name.replace(/[-\W]+$/, ''))
+      .forEach(channel => {
+        channel.fetchMessages({
+        	limit: 100 // Fetch last 100 messages.
+        }).then((messages) => {
+          const name = channel.name.replace(/[-\W]+$/, '');
+          pokemonList[name] = {
+            channel: '' + channel,
+            channelName: channel.name,
+            dateStr: '',
+            symbol: statusSymbols['unconfirmed'],
           }
-      	});
-        if (++i >= channels.size){
-          resolve(pokemonList);
-          debug("Fetched latest shiny list");
-        }
-      }).catch(e => {
-        if (++i >= channels.size){
-          resolve(pokemonList);
-          debug("Fetched latest shiny list");
-        }
-        switch (e.message){
-          case 'Missing Access':
-            break;
-          default:
-            error('Failed to fetch messages for this channel:\n', `\tMessage: ${e.message}\n`, `\tError No: ${e.errno}\n`, `\tCode: ${e.code}\n`);
-        }
+        	messages.forEach((msg) => {
+        		if (msg.pinned == true && isMatch.test(msg.content)){
+              const date = new Date(Date.parse(msg.content.match(isMatch)[2]))
+              pokemonList[name] = {
+                ...pokemonList[name],
+                ...{
+                  date,
+                  dateStr: msg.content.match(isMatch)[2],
+                  symbol: getSymbolFromDate(date),
+                },
+              }
+              return;
+            }
+        	});
+          if (++i >= channels.size){
+            resolve(pokemonList);
+            debug("Fetched latest shiny list");
+          }
+        }).catch(e => {
+          if (++i >= channels.size){
+            resolve(pokemonList);
+            debug("Fetched latest shiny list");
+          }
+          switch (e.message){
+            case 'Missing Access':
+              break;
+            default:
+              error('Failed to fetch messages for this channel:\n', `\tMessage: ${e.message}\n`, `\tError No: ${e.errno}\n`, `\tCode: ${e.code}\n`);
+          }
+        });
       });
-    });
   });
 }
 
