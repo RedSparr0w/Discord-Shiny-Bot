da30cf7c4023529c7e3a9d0a14ec339f5fbabc80
Danial Nickford <admin@redsparr0w.com>
2019-07-18 11:23:40 +1200

    [startup] Remove startup scripts

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const {
   updateChannelNames,
 } = require('./helpers.js');
 const { setupDB } = require('./database.js');
-const startup = require('./startup.js');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -25,7 +24,6 @@ const cooldowns = new Discord.Collection();
 client.once('ready', async() => {
   info(`Logged in as ${client.user.tag}!`);
   await setupDB();
-  await startup(client);
 
   // Start our functions that run on the hour, when the timer next reaches the closest hour
   setTimeout(()=>{
