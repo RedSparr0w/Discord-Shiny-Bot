1897fbfe2c7e260052f1506ae66aef321299a853
Danial Nickford <admin@redsparr0w.com>
2019-07-19 09:13:41 +1200

    [report] delete messages after 10 seconds

--- commands/reports.js
+++ commands/reports.js
@@ -1,3 +1,4 @@
+const { error } = require('../helpers.js');
 const { getUserReports } = require('../database.js');
 
 module.exports = {
@@ -10,7 +11,12 @@ module.exports = {
   botperms    : ['SEND_MESSAGES'],
   userperms   : ['SEND_MESSAGES'],
   execute     : async (msg, args) => {
+    msg.delete().catch(e=>error('Unable to delete message:\n', `\tMessage: ${e.message}\n`, `\tError No: ${e.errno}\n`, `\tCode: ${e.code}\n`));
     const reports = await getUserReports(msg.member.id);
-    return msg.reply(`You have made ${reports} reports!`);
+    return msg.reply(`You have made ${reports} reports!`).then(m=>{
+      setTimeout(()=>{
+        m.delete().catch(e=>error('Unable to delete message:\n', `\tMessage: ${e.message}\n`, `\tError No: ${e.errno}\n`, `\tCode: ${e.code}\n`));
+      }, 10000);
+    });
   }
 };
