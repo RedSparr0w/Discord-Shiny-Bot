1a1752b5d1fcb895474939446e3a96d719809b59
Danial <admin@redsparr0w.com>
2022-03-10 13:55:17 +1300

    Add more comments

--- other/ocr.js
+++ other/ocr.js
@@ -13,10 +13,12 @@ let tesseractInstalled = false;
 })();
 
 const extractMessageDate = async (files) => {
+  const errorDate = new Date(0);
   // Check if tesseract installed
-  if (!tesseractInstalled) return new Date(0);
+  if (!tesseractInstalled) return errorDate;
   // Proccess the data
   try {
+    // Get out numbers and /- characters from out image
     let output = '';
     for (const file of files) {
       output += await recognize(file, {
@@ -25,12 +27,20 @@ const extractMessageDate = async (files) => {
     }
     output = output?.split('\n').join(' ');
 
+    // Try find a date in the text
     let [month, day, year] = (output.match(/(\d{1,2}\/\d{1,2}\/\d{4}|\d{1,2}-\d{1,2}-\d{4})/i)?.[0] || '').split(/[-/]/);
 
+    // If no date, try with year first
     if (day == undefined || month == undefined || year == undefined) {
       [year, month, day] = (output.match(/(\d{4}-\d{1,2}-\d{1,2})/i)?.[0] || '').split(/[-/]/);
     }
 
+    // If no date still, just return
+    if (day == undefined || month == undefined || year == undefined) {
+      return errorDate;
+    }
+
+    // Calculate date with month/day swapped around
     const now = new Date();
     const date1 = new Date(`${year}-${month}-${day}`);
     const date2 = new Date(`${year}-${day}-${month}`);
@@ -41,9 +51,9 @@ const extractMessageDate = async (files) => {
     const date = isNaN(date1) ? date2 : isNaN(date2) || Math.abs(now - +date1) < Math.abs(now - +date2) ? date1 : date2;
 
     // Return 0 date or date we got
-    return !+date ? new Date(0) : date;
+    return !+date ? errorDate : date;
   } catch(e){
-    return new Date(0);
+    return errorDate;
   }
 };
 
